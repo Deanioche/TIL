f76c39d905c866184408dd24b7b8d3c581112337
Soksurim <briochefavori@gmail.com>
2021-06-19 23:43:19 +0900

    JS test

--- Javascript/test.js
+++ Javascript/test.js
@@ -4,4 +4,31 @@ function A(n, a) {
 
 const a = new A();
 
-console.log(a, typeof a);
+//console.log(a, typeof a);
+
+const bb = 5
+
+{
+const bb = 3 + 5
+console.log(bb)
+
+}
+console.log(bb);
+
+(function() {
+    console.log("즉시 실행 함수")
+})()
+
+let x = function(){
+    console.log("x 익명 함수")
+    return "[x() 리턴값]"
+}
+
+console.log("x() 실행 결과 : " + x())
+
+let y = function ss(){
+    console.log("y 기명 함수 ss")
+    return "[y() 리턴값]"
+}
+
+console.log("y() 실행 결과 : " + y())
