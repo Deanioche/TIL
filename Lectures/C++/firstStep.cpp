47150e7614dd2939236711677b962073c7cd4f26
Soksurim <briochefavori@gmail.com>
2021-10-12 22:02:43 +0900

    c++ intro, namespace

---
+++ Lectures/C++/firstStep.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    int a = 100;
+    // 표준 출력 스트림으로 문장을 출력함
+    std::cout << "나의 첫 번째 C++ 프로그램\n";
+    std::cout << "a의 값은\n"
+              << a << "\n입니다."
+              << std::endl;
+    return 0;
+}
---
+++ Lectures/C++/namespace.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std; // namespace 사용 선언
+namespace myNSp1
+{
+    int n = 10;
+}
+namespace myNSp2
+{
+    int n = 20;
+}
+int n = 30;
+
+int main()
+{
+    int n = 40;
+    cout << myNSp1::n << endl;
+    cout << myNSp2::n << endl;
+    cout << ::n << endl;
+    cout << n << endl;
+    return 0;
+}
