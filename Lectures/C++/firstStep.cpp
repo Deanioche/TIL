f5b7987ac67195ebe25164e8f84058cb26a79398
Soksurim <briochefavori@gmail.com>
2021-10-14 00:36:05 +0900

    added cases

--- Lectures/C++/firstStep.cpp
+++ Lectures/C++/firstStep.cpp
@@ -6,6 +6,7 @@ int main()
     std::cout << "나의 첫 번째 C++ 프로그램\n";
     std::cout << "a의 값은\n"
               << a << "\n입니다."
-              << std::endl;
+              << std::ends; // 줄바꿈 x. Null만 입력됨.
+    std::cout << "123";     // end를 하지 않아도 출력은 된다.
     return 0;
 }
